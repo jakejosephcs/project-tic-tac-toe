63c31bab852540f08d279a7fb24d57dcb13f4c19
Jake Joseph <jakejosephcs@gmail.com>
2020-04-25 15:26:58 -0400

    Cleaned up code, to add AI using minimax algorithm

--- scripts/game.js
+++ scripts/game.js
@@ -1,81 +1,38 @@
 function init(player, OPPONENT){
+    // We'll need to grab out canvas element in order to manipulate it
     const canvas = document.getElementById('cvs');
+
+    // We also need to create the context of the canvas (2D in our case)
     const ctx = canvas.getContext('2d');
+
+    // We'll have a 3x3 grid so we'll define our rows and columns
     const COLUMN = 3;
     const ROW = 3;
 
+    // We also have to define our board which will be a 2D array of [rows[columns]]
     let board = [];
 
+    /* Each cell in our grid we'll have a fixed width and height of 150px since we
+       defined our canvas to 450px by 450px */
     const SPACE_SIZE = 150;
 
-    function drawBoard(){
-        let id = 0;
-        for(let i = 0; i < ROW; i++){
-            board[i] = [];
-            for(let j = 0; j < COLUMN; j++){
-                board[i][j] = id;
-                ctx.strokeStyle = "#000";
-                ctx.strokeRect(j*SPACE_SIZE, i*SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
-                id++;
-            };
-        };
-    };
-    drawBoard();  
-    
+    /* As the player clicks on a cell, we'll need to store where they clicked so 
+       we'll create a gameData array that has 9 slots, one for each cell */
     let gameData = new Array(9);
+
+    // The first move will be given to the user and not the friend/computer
     let currentPlayer = player.man;
+
+    // We'll need to keep track if the game is over or not in our isWinner() or isTie() functions
     let GAME_OVER = false;
-    canvas.addEventListener('click', (e) => {
-        if(GAME_OVER){
-            return;
-        }
 
-        let X = e.clientX - canvas.getBoundingClientRect().x;
-        let Y = e.clientY - canvas.getBoundingClientRect().y;
-
-        let j = Math.floor(X / SPACE_SIZE);
-        let i = Math.floor(Y / SPACE_SIZE);
-
-        let id = board[i][j];
-
-        if (gameData[id]){
-            return;
-        };
-
-        gameData[id] = currentPlayer
-
-        drawOnBoard(currentPlayer, i, j);
-
-        if(isWinner(currentPlayer, gameData)){
-            showGameOver(currentPlayer)
-            GAME_OVER = true;
-            return;
-        };
-
-        if(isTie(gameData)){
-            showGameOver("tie");
-            GAME_OVER = true;
-            return;
-        }
-
-        if(currentPlayer == player.man) {
-            currentPlayer = player.friend;
-        }else {
-            currentPlayer = player.man;
-        }
-        
-    });
-
+    // When the user clicks their chosen cell, we want to display either X or O
     const xImage = new Image();
     xImage.src = "images/X.png";
     const oImage = new Image();
     oImage.src = "images/O.png";
 
-    function drawOnBoard(player, i, j){
-        let img = player == "X" ? xImage : oImage;
-        ctx.drawImage(img, j*SPACE_SIZE, i*SPACE_SIZE);
-    }
-
+    // We'll have to check if a user has won or not. We'll hold the winning combination in a 2D array
     const COMBOS = [
         [0, 1, 2],
         [3, 4, 5],
@@ -86,7 +43,85 @@ function init(player, OPPONENT){
         [0, 4, 8],
         [2, 4, 6]
     ];
+    // To display the game over message, we'll need to grab the .gameover class to manipulate it
+    const gameOverElement = document.querySelector('.gameover');
 
+    // When we first switch to the cavas, we'll want to draw the board
+    function drawBoard(){
+        // We'll also want to assign each cell an ID for future use
+        let id = 0;
+        // Iterating through our rows variable and each time creating a new array in our board variable
+        for(let i = 0; i < ROW; i++){
+            board[i] = [];
+            /* Each time we iterate through a column inside a row, we'll assign it an ID as well as 
+               draw the outline of the cell and then increment out id */
+            for(let j = 0; j < COLUMN; j++){
+                board[i][j] = id;
+                ctx.strokeStyle = "#000";
+                ctx.strokeRect(j*SPACE_SIZE, i*SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
+                id++;
+            };
+        };
+    };
+
+    // Now we can draw our board
+    drawBoard();  
+    
+    // When we click on the canvas we'll trigger an event to add either an X or O to it
+    canvas.addEventListener('click', (e) => {
+        // First we'll check if the GAME_OVER variable has changed from true to false, if it has we'll exit the function
+        if(GAME_OVER){ return; };
+
+        // When the user clicks the canvas, we'll need to get where they clicked
+        let X = e.clientX - canvas.getBoundingClientRect().x;
+        let Y = e.clientY - canvas.getBoundingClientRect().y;
+
+        // We'll want to convert our X and Y values into i and j values to assign an ID
+        let j = Math.floor(X / SPACE_SIZE);
+        let i = Math.floor(Y / SPACE_SIZE);
+        let id = board[i][j];
+
+        // If the ID already exits in our gameData array, we'll exit the function otherwise, we'll assign it
+        if (gameData[id]){
+            return;
+        } else  {
+            gameData[id] = currentPlayer
+        }
+
+        /* Once the user has clicked a cell that has no already been assigned, we'll draw their choice (X or O)
+           onto the board */
+        drawOnBoard(currentPlayer, i, j);
+
+        // Once the current player draws on the board we have to check if there is a winner
+        if(isWinner(currentPlayer, gameData)){
+            showGameOver(currentPlayer)
+            GAME_OVER = true;
+            return;
+        };
+
+        // If there is no winner, we'll check to see if there is a tie
+        if(isTie(gameData)){
+            showGameOver("tie");
+            GAME_OVER = true;
+            return;
+        }
+
+        // If it is none of the above, we'll switch the currentPlayer's turn
+        if(currentPlayer == player.man) {
+            currentPlayer = player.friend;
+        }else {
+            currentPlayer = player.man;
+        }
+        
+    });
+
+    // The draw on board takes the current player and the location they clicked and uses that to draw on the canvas
+    function drawOnBoard(player, i, j){
+        let img = player == "X" ? xImage : oImage;
+        ctx.drawImage(img, j*SPACE_SIZE, i*SPACE_SIZE);
+    }
+
+    // We check if there is a winner by looping through our COMBOS array and checking if the gameData array matches a combo
     function isWinner(player, gameData){
         for(let i = 0; i < COMBOS.length; i++){
             let won = true;
@@ -101,6 +136,7 @@ function init(player, OPPONENT){
         return false;
     }
 
+    // We check for a tie by checking if the board is full
     function isTie(gameData) {
         let isBoardFull = true;
         for (let i = 0; i < gameData.length; i++) {
@@ -112,11 +148,10 @@ function init(player, OPPONENT){
         return false
     }
 
-    const gameOverElement = document.querySelector('.gameover');
-
+    // Once  the game is over, we display who the winner is or if there was a tie
     function showGameOver(player){
-        let imgSrc = `images/${player}.png`
-        let message = player == "tie" ? "Opps No Winner!" : "The Winner is"
+        let imgSrc = `images/${player}.png`;
+        let message = player == "tie" ? "Opps No Winner!" : "The Winner is";
 
         gameOverElement.innerHTML = `
             <h1>${message}</h1>
--- scripts/options.js
+++ scripts/options.js
@@ -1,3 +1,4 @@
+// Grabbing all of our classes and assigning them to variables
 const options = document.querySelector('.options');
 const computerBtn = document.querySelector('.computer');
 const friendBtn = document.querySelector('.friend');
@@ -5,53 +6,69 @@ const xBtn = document.querySelector('.x');
 const oBtn = document.querySelector('.o');
 const playBtn = document.querySelector('.play');
 
+// Since we'll have a player and an opponent, we'll create an object and variable
 const player = new Object;
 let OPPONENT;
 
-function switchActive(off, on){
-    off.classList.remove('active');
-    on.classList.add('active');
-}
-
-
+// When the user clicks the COMPUTER button
 computerBtn.addEventListener('click', () => {
+    // Since the opponent is playing with a computer, opponent get assigned computer
     OPPONENT = 'computer';
     switchActive(friendBtn, computerBtn);
 });
-// ===========
+
+// When the user clicks the FRIEND button
 friendBtn.addEventListener('click', () => {
+    // Since the opponent is playing with a friend, opponent get assigned friend
     OPPONENT = 'friend';
     switchActive(computerBtn, friendBtn);
 });
-// ===========
+
+// When the user clicks the O button
 oBtn.addEventListener('click', () => {
+    // The choice of the player becomes O and the opponent (player or friend) is X
     player.man = "O";
     player.computer = "X";
     player.friend = "X";
     switchActive(xBtn, oBtn);
 });
-// ===========
+
+// When the user clicks the X button
 xBtn.addEventListener('click', () => {
+    // The choice of the player becomes X and the opponent (player or friend) is O
     player.man = "X";
     player.computer = "O";
     player.friend = "O";
     switchActive(oBtn, xBtn);
 });
-// ===========
+
+// When the user clicks the PLAY button
 playBtn.addEventListener('click', () => {
+    /* We need to check if the user selected either COMPUTER or OPPONENT
+       This can be done by checking is OPPONENT has been assigned */
     if(!OPPONENT) {
         computerBtn.style.backgroundColor = "red";
         friendBtn.style.backgroundColor = "red";
         return;
     }
-
+    /* We need to check if the user selected either X or O
+       This can be done by checking is player.man has been assigned */
     if(!player.man) {
         oBtn.style.backgroundColor = "red";
         xBtn.style.backgroundColor = "red";
         return;
     }
 
+    // If all selections are valid, we'll start the game (game.js)
     init(player, OPPONENT);
     
+    // Once the game is started we'd like to hide the options page
     options.classList.add('hide');
 });
+
+// We need to switch each button from active to nonactive once we pick/switch an option
+function switchActive(off, on){
+    off.classList.remove('active');
+    on.classList.add('active');
+    off.style.backgroundColor = "white";
+};
